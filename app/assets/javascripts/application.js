17cbcbd6b1bf320f63958a5db63749a34453e3ff
Max Melentiev <melentievm@gmail.com>
2017-11-23 19:40:31 +0300

    Add example

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,59 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+//= require vue/dist/vue
+
+window.START = +new Date();
+function secFromStart() {
+  return Math.round((new Date() - window.START) / 1000);
+}
+
+function buildVue(element) {
+  new Vue({
+    el: element,
+    created: function(){
+      this.interval = setInterval(() => { console.log(secFromStart()) }, 1000)
+    },
+    beforeDestroy: function(){
+      if (this.interval) { clearInterval(this.interval) }
+    }
+  })
+}
+
+// Plugin adapted for sprockets.
+(function(){
+
+function handleVueDestructionOn(turbolinksEvent, vue) {
+  document.addEventListener(turbolinksEvent, function teardown() {
+    vue.$destroy();
+    document.removeEventListener(turbolinksEvent, teardown);
+  });
+}
+
+function plugin(Vue, options) {
+  // Install a global mixin
+  Vue.mixin({
+
+    beforeMount: function() {
+      // If this is the root component, we want to cache the original element contents to replace later
+      // We don't care about sub-components, just the root
+      if (this == this.$root) {
+        handleVueDestructionOn('turbolinks:visit', this);
+        this.$originalEl = this.$el.outerHTML;
+      }
+    },
+
+    destroyed: function() {
+      // We only need to revert the html for the root component
+      if (this == this.$root) {
+        this.$el.outerHTML = this.$originalEl;
+      }
+    }
+  })
+};
+
+// export default plugin;
+Vue.use(plugin)
+
+})()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :sample, only: [], controller: :samples do
+    get :with_vue, :without_vue
+  end
+
+  root to: 'samples#without_vue'
 end
